7e51b4f36299003859c313c714650d9d7d916241
BrandonVassallo <bav5252@psu.edu>
2026-04-15 23:48:58 -0400

    Finished UI Setup, Added exit confirmation where all fields marked with a "*" must be filled before confirmation.
    Also added a cancel button

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ setup:s
     - See manage_config.py
 '''
 waybar_pos = "LEFT"
+color = 0
 
 def prompt_user():
 
@@ -38,6 +39,7 @@ def prompt_user():
 
 
     def color_picker():
+        global color
         color = colorchooser.askcolor(title="Choose Icon Color")
 
         # The color is represneted as a tuple: ((R, G, B), "#rrbbgg")
@@ -87,7 +89,41 @@ def prompt_user():
             tool_label.grid()
             tool_entry.grid()
 
+    def update_dropdown_bool():
+        if drop_bool.get() == False:
+            # Turn off tooltip entry
+            drop_label.grid_remove()
+            drop_entry.grid_remove()
+        else:
+            # Turn on tooltip entry
+            drop_label.grid()
+            drop_entry.grid()
 
+    def check_exit():
+        if icon_name.get() != "" and nerd_icon.get() != "" and command.get() != "" and color != 0:
+            # RUN CONFORMATION CODE
+            root.destroy()
+        else:
+            if icon_name.get() == "":
+                icon_name_label.config(fg="Red")
+            else:
+                icon_name_label.config(fg="Black")
+
+            if nerd_icon.get() == "":
+                nerd_label.config(fg="Red")
+            else:
+                nerd_label.config(fg="Black")
+
+            if command.get() == "":
+                command_label.config(fg="Red")
+            else:
+                command_label.config(fg="Black")
+
+            if color == 0:
+                color_label.config(fg="Red")
+            else:
+                color_label.config(fg="Black")
+                
 
     ########################
     ### Open User Window ###
@@ -102,7 +138,8 @@ def prompt_user():
 
     # ROW 1 - Icon Decleration
     icon_name = StringVar()
-    ttk.Label(window, text="Icon Decleration Name").grid(column=0,row=1, sticky=E)
+    icon_name_label = ttk.Label(window, text="Icon Decleration Name*")
+    icon_name_label.grid(column=0,row=1, sticky=E)
     icon_name_entry = ttk.Entry(window, width=20, textvariable=icon_name)
     icon_name_entry.grid(column=1, row=1, sticky=W)
 
@@ -110,18 +147,21 @@ def prompt_user():
     # ROW 2 - Nerd Font Icon
     nerd_font = font.Font(family="JetBrainsMono Nerd Font")
     nerd_icon = StringVar()
-    ttk.Label(window, text="Nerd Font Icon").grid(column=0,row=2, sticky=E)
+    nerd_label = ttk.Label(window, text="Nerd Font Icon*")
+    nerd_label.grid(column=0,row=2, sticky=E)
     nerd_font_entry = ttk.Entry(window, font=nerd_font, width=3, textvariable=nerd_icon)
     nerd_font_entry.grid(column=1, row=2, sticky=W)
 
     # ROW 3 - Command
     command = StringVar()
-    ttk.Label(window, text="Command to be run").grid(column=0,row=3, sticky=E)
+    command_label = ttk.Label(window, text="Command to be run*")
+    command_label.grid(column=0,row=3, sticky=E)
     command_entry = ttk.Entry(window, width=40, textvariable=command)
     command_entry.grid(column=1, row=3, sticky=W)
 
     # ROW 4 - Icon Color
-    ttk.Label(window, text="Choose Icon Color:").grid(column=0,row=4, sticky=E)
+    color_label = ttk.Label(window, text="Choose Icon Color:*")
+    color_label.grid(column=0,row=4, sticky=E)
     selected_color = ttk.Label(window, text="No color selected")
     selected_color.grid(column=2, row=4, sticky=E)
     ttk.Button(window, text="Pick a Color", command=lambda: color_picker()).grid(column=1, row=4, sticky=W)
@@ -152,7 +192,7 @@ def prompt_user():
     tool_entry = ttk.Entry(window, width=40, textvariable=tooltip)  # In ROW 11, but needs to be defined before update_tooltip_bool
 
 
-    ttk.Label(window, text="Include a Tooltip?:").grid(column=0,row=10, sticky=E)
+    ttk.Label(window, text="Include a Tooltip?").grid(column=0,row=10, sticky=E)
     tool_check_box = ttk.Checkbutton(window, variable=tool_bool, command=update_tooltip_bool)
     tool_check_box.grid(column=1, row=10, sticky=W)
 
@@ -163,13 +203,26 @@ def prompt_user():
     update_tooltip_bool()
 
     # ROW 12 - Dropdown?
+    drop_bool = ttk.BooleanVar(value=False)
+    drop_label = ttk.Label(window, text="Dropdown Number:")                 # In ROW 13, but needs to be defined before update_tooltip_bool
+    drop_num = ttk.StringVar()
+    drop_entry = ttk.Entry(window, width=6, textvariable=drop_num)                      # In ROW 13, but needs to be defined before update_tooltip_bool
 
-    # ROW 13 - Empty
-    ttk.Label(window, text=" ").grid(column=0, row=13)
+    ttk.Label(window, text="Include a Dropdown?").grid(column=0,row=12, sticky=E)
+    drop_check_box = ttk.Checkbutton(window, variable=drop_bool, command=update_dropdown_bool)
+    drop_check_box.grid(column=1, row=12, sticky=W)
 
-    # ROW 14 - Exit Button
+    # ROW 13 - Number of dropdown elements
+    drop_entry.grid(column=1, row=13, sticky=W)
+    drop_label.grid(column=0, row=13, sticky=E)
 
+    update_dropdown_bool()
 
+    # ROW 14 - Empty
+    ttk.Label(window, text=" ").grid(column=0, row=14)
+
+    # ROW 15 - Exit Button
+    ttk.Button(window, text="Finished?", command=check_exit).grid(column=0, row=15)
 
     window.mainloop()
 
